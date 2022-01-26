92c791dec173c211d1df30c63ef3eb2171ad6301
Yigit S <55548182+yigitsr@users.noreply.github.com>
2022-01-26 15:20:24 +0300

    Delete src directory

--- src/script.js
+++
@@ -1,5 +0,0 @@
-// alert('test')
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul');
-
-hamburger.addEventListener('click', () => { navUL.classList.toggle('show'); });
